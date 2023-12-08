d63fcc206f48981bc32281a58b837a562f062ba2
Umar Khan <ukhan57@myseneca.ca>
2023-12-08 01:27:01 -0500

    Fixing the retrieval of tokens in userData.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 /**********************************************************************************
- *  WEB422 – Assignment 6* 
- * I declare that this assignment is my own work in accordance with Seneca Academic Policy.* 
- * No part of this assignment has been copied manually or electronically from any other source* 
- * (including web sites) or distributed to other students.*
- * 
- * Name: Umar Khan Student ID: 145270211 Date: 12/07/2023
- * **********************************************************************************/
-
+ *  WEB422 – Assignment 06*
+ *  I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+ *  assignment has been copied manually or electronically from any other source (including web sites) or
+ *  distributed to other students.
+ *  Name: Umar Khan Student ID: 145270211 Date: 12/07/2023
+ *  Vercel App (Deployed) Link: https://web422-assignment6-next-project.vercel.app/
+ * *********************************************************************************/
 import { Container, Row, Col, Image } from 'react-bootstrap';
 
 export default function Home() {
